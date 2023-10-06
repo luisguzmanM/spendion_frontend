ae912f06fd8ce585d23fd5c38f8befc3c5397622
Luis Guzman <guzmanluis.lg@gmail.com>
2023-10-06 18:16:21 -0500

    update

--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { Budget } from 'src/app/models/budget.model';
 import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
