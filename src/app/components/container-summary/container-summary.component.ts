d8b50af92c7d3ffe66da7e5ff4fa3327899ab797
Luis Guzman <guzmanluis.lg@gmail.com>
2023-11-01 14:07:41 -0500

    update icons balance

--- src/app/components/container-summary/container-summary.component.ts
+++ src/app/components/container-summary/container-summary.component.ts
@@ -64,7 +64,7 @@ export class ContainerSummaryComponent implements OnInit {
       {
         title: 'Balance',
         amount: this.balance,
-        icon: '💰'
+        icon: '⚖️'
       }
     ]
   }
