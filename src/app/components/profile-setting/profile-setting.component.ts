b594d6298eacaa2dfa551371ddbf20a5de7e46a5
Luis Guzman <guzmanluis.lg@gmail.com>
2023-11-19 16:24:19 -0500

    update

--- src/app/components/profile-setting/profile-setting.component.ts
+++ src/app/components/profile-setting/profile-setting.component.ts
@@ -6,11 +6,9 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { SettingService } from 'src/app/services/setting.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { UtilsService } from 'src/app/services/utils.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-setting',
@@ -35,18 +33,18 @@ import { Router } from '@angular/router';
 export class ProfileSettingComponent implements OnInit {
 
   value: string;
+  loading: boolean = false;
+  user;
+
   formCtrl = new FormGroup({
     firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
     lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
     photo: new FormControl(null),
   })
-  loading: boolean = false;
-  user;
 
   constructor(
     private _settingSvc: SettingService,
-    private _utilsSvc: UtilsService,
-    private _router: Router
+    private _utilsSvc: UtilsService
   ){
 
   }
@@ -74,7 +72,6 @@ export class ProfileSettingComponent implements OnInit {
   }
 
   handleUpdateUserDataResponse(data){
-    console.log(data)
     this.loading = false;
     localStorage.removeItem('person');
     localStorage.setItem('person', JSON.stringify(data.person));
@@ -82,7 +79,6 @@ export class ProfileSettingComponent implements OnInit {
   }
 
   handleUpdateUserDataError(error){
-    console.log(error)
     this.loading = false;
     this._utilsSvc.openSnackBar('Error trying to update user data', 'Close');
   }
