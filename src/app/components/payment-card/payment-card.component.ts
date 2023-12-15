f44801b6ef76a73cd3f6ccced80e689fcd7fc62b
Luis Guzman <guzmanluis.lg@gmail.com>
2023-12-14 20:45:05 -0500

    create subscription system

--- src/app/components/payment-card/payment-card.component.ts
+++ src/app/components/payment-card/payment-card.component.ts
@@ -4,6 +4,7 @@ import { PaymentComponent } from '../payment/payment.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { SubscriptionService } from 'src/app/services/subscription.service';
 
 @Component({
   selector: 'app-payment-card',
@@ -17,7 +18,39 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatIconModule,
   ],
+  providers: [
+    SubscriptionService
+  ]
 })
 export class PaymentCardComponent {
+
+  constructor(
+    private _subscriptionSvc: SubscriptionService
+  ){}
+
+  createProduct(){
+    this._subscriptionSvc.createProduct().subscribe(res => {
+      console.log('ID product :', res.data.id);
+      this.createPlan(res.data.id);
+    })
+  }
+
+  createPlan(idProduct:string){
+    this._subscriptionSvc.createPlan(idProduct).subscribe(res => {
+      console.log('ID plan: ', res.data.id);
+      this.createSubscription(res.data.id);
+    })
+  }
+
+  createSubscription(idPlan:string){
+    this._subscriptionSvc.createSubscription(idPlan).subscribe(res => {
+      this.redirectUserToPaymentPage(res.data.links);
+    })
+  }
+
+  redirectUserToPaymentPage(links:any):void {
+    links = links.filter(link => link.rel === 'approve' && link.method === 'GET');
+    window.location.href = links[0].href;
+  }
 
 }
---
+++ src/app/services/subscription.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_URL } from '../globals';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubscriptionService {
+
+  URL_BACKEND: string = API_URL + '/payment';
+
+  constructor(
+    private _httpClient : HttpClient
+  ) { }
+
+  createProduct():Observable<any>{
+    return this._httpClient.post(`${this.URL_BACKEND}/createProduct`, null);
+  }
+
+  createPlan(idProduct:string):Observable<any>{
+    return this._httpClient.post(`${this.URL_BACKEND}/createPlan`, {product_id: idProduct});
+  }
+
+  createSubscription(idPlan:string):Observable<any>{
+    return this._httpClient.post(`${this.URL_BACKEND}/createSubscription`, {plan_id: idPlan})
+  }
+}
