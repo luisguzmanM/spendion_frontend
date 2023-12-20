7c8093b41bb0fba24a5ec9e020364da3f8b1f455
Luis Guzman <guzmanluis.lg@gmail.com>
2023-12-19 20:28:18 -0500

    update

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,2 +1,1 @@
-export const API_URL = window.location.href.includes('localhost') ? 'http://localhost:4200' : 'https://spendion-backend.onrender.com';
-console.log('API_URL: ', API_URL)
+export const API_URL = window.location.href.includes('localhost') ? 'http://localhost:4000' : 'https://spendion-backend.onrender.com';
